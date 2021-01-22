8b0ab24a6c0562b7fdd4c7f205377f92a28ad677
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-01-22 22:56:40 +0900

    030_メソッドとプロパティー

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,7 @@
+console.log()
+
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+console.log(typeof variable);
---
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,10 @@
+
+
+function hello(name, age) {
+  // let name = 'Code Mafia';
+console.log('hello ' + name + age);
+return name + age;
+}
+
+hello('Code Mafia, 10');
+hello('Code Mafia2, 20');
