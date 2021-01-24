fd304ee3cb26c6685475129730a11afd47c96769
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-01-24 14:46:30 +0900

    080_DOMを操作してみよう

--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -2,8 +2,20 @@ const str = 'animation';
 const strArry = str.split('');
 
 function tag(accu, curr) {
+    console.log(accu, curr);
     return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+
+function reduce(arry, callback, defaultValue) {
+    let accu = defaultValue;
+    for(let i = 0; i < arry.length; i++) {
+        let curr = arry[i];
+        accu = callback(accu, curr);
+    }
+
+    return accu;
+}
+
+const result = reduce(strArry, tag, "");
 console.log(result);
