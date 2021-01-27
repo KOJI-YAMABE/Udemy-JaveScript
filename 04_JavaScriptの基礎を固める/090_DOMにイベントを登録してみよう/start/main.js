f22051d8c3eaea83e21b30e51a2b90598c6c7c83
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-01-27 16:49:41 +0900

    111_classを用いてコード再利用性を高めよう

---
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn');
+
+const hello = function() {
+  alert('hello');
+};
+
+btn.addEventListener('click', hello);
--- 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
+++ 04_JavaScriptの基礎を固める/111_classを用いてコード再利用性を高めよう/start/main.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const el = document.querySelector('.animate-title');
-    const el2 = document.querySelector('.animate-title-2');
-    const str = el.innerHTML.trim().split("");
-    const str2 = el2.innerHTML.trim().split("");
-    
-    // let concatStr = '';
-
-    // for(let c of str) {
-    //     c = c.replace(/\s+/, '&nbsp;');
-    //     concatStr += `<span class="char">${c}</span>`;
-    // }
-
-    el.innerHTML = str.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-    }, "");
-    el2.innerHTML = str2.reduce((acc, curr) => {
-        curr = curr.replace(/\s+/, '&nbsp;');
-        return `${acc}<span class="char">${curr}</span>`;
-    }, "");
+    const ta = new TextAnimation('.animate-title');
+    const ta2 = new TextAnimation('.animate-title-2');
+    setTimeout(() => {
+        ta.animate();
+        ta2.animate();
+    }, 1000);
 });
+
+class TextAnimation {
+    constructor(el) {
+        this.el = document.querySelector(el);
+        this.chars = this.el.innerHTML.trim().split("");
+        this.el.innerHTML = this._splitText();
+    }
+    _splitText() {
+        return this.chars.reduce((acc, curr) => {
+            curr = curr.replace(/\s+/, '&nbsp;');
+            return `${acc}<span class="char">${curr}</span>`;
+        }, "");
+    }
+    animate() {
+        this.el.classList.toggle('inview');
+    }
+}
