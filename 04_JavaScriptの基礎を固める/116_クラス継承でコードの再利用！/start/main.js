7b72294ca5e86eb9c1be4dcb1b019625e9ffb10f
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-02-02 00:29:05 +0900

    JaveScript基礎終了

--- 04_JavaScriptの基礎を固める/116_クラス継承でコードの再利用！/start/main.js
+++ 04_JavaScriptの基礎を固める/116_クラス継承でコードの再利用！/start/main.js
@@ -18,21 +18,22 @@ class TextAnimation {
         }, "");
     }
     animate() {
-        this.el.classList.toggle('inview');
+        this.DOM.el.classList.toggle('inview');
     }
 }
-class TweenTextAnimation extends TextAnimation  {
+class TweenTextAnimation extends TextAnimation {
     constructor(el) {
         super(el);
         this.DOM.chars = this.DOM.el.querySelectorAll('.char');
     }
+    
     animate() {
         this.DOM.el.classList.add('inview');
         this.DOM.chars.forEach((c, i) => {
             TweenMax.to(c, .6, {
                 ease: Back.easeOut,
                 delay: i * .05,
-                startAt: {y: '-50%', opacity: 0},
+                startAt: { y: '-50%', opacity: 0},
                 y: '0%',
                 opacity: 1
             });
