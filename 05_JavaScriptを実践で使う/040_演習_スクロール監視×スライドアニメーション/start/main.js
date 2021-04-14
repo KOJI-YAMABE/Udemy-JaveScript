16281995d79120bbc6deeb45d21cc462f4df1e06
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-04-14 15:29:22 +0900

    050_モバイル用メニューを作成しよう

--- 05_JavaScriptを実践で使う/040_演習_スクロール監視×スライドアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/040_演習_スクロール監視×スライドアニメーション/start/main.js
@@ -2,11 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const cb = function (el, isIntersecting) {
         if(isIntersecting) {
-            const ta = new TextAnimation(el);
-            ta.animate();
+            el.classList.add('inview');
         }
     }
 
-    const so = new ScrollObserver('.animate-title', cb);
+    const so = new ScrollObserver('.cover-slide', cb);
 });
 
