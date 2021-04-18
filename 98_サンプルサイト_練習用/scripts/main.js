b8a4beff39aa093ea98397319cd584e6af9cd076
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-04-18 11:04:35 +0900

    Flexbox

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -2,12 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
   const hero = new HeroSlider('.swiper-container');
   hero.start();
 
-  const cb = function (el, isIntersecting) {
-    if(isIntersecting) {
+  const cb = function (el, inview) {
+    if(inview) {
         const ta = new TweenTextAnimation(el);
         ta.animate();
     }
   }
 
   const so = new ScrollObserver('.tween-animate-title', cb);
+
+  const _inviewAnimation = function(el, inview) {
+    if (inview) {
+      el.classList.add('inview');
+    } else {
+      el.classList.remove('inview');
+    }
+  }
+
+  const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
